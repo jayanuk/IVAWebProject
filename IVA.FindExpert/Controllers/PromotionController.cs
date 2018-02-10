86d4abcd48c1adf1188e75bfe504023229b5ce05
Amila Pradeep <amilapradeep@gmail.com>
2018-02-10 14:36:41 +0400

    Null reference issue fix due to no promotion on database

--- IVA.FindExpert/Controllers/PromotionController.cs
+++ IVA.FindExpert/Controllers/PromotionController.cs
@@ -1,6 +1,8 @@
-﻿using IVA.Common;
+﻿using Core.Log;
+using IVA.Common;
 using IVA.DbAccess;
 using IVA.DbAccess.Repository;
+using IVA.FindExpert.Helpers.ActionFilters;
 using System;
 using System.Collections.Generic;
 using System.Linq;
@@ -14,6 +16,7 @@ namespace IVA.FindExpert.Controllers
     {
         [HttpGet]
         [Authorize]
+        [AccessActionFilter]
         public IHttpActionResult GetPromotions(int Type)
         {
             List<PromotionModel> promotions = null;
@@ -36,6 +39,7 @@ namespace IVA.FindExpert.Controllers
             }
             catch(Exception ex)
             {
+                Logger.Log(typeof(PromotionController), ex.Message + ex.StackTrace, LogType.ERROR);
                 return InternalServerError(ex);
             }
 
@@ -44,6 +48,7 @@ namespace IVA.FindExpert.Controllers
 
         [HttpGet]
         [Authorize]
+        [AccessActionFilter]
         public IHttpActionResult GetPromotionById(int Id)
         {
             PromotionModel promotion = null;
@@ -52,20 +57,25 @@ namespace IVA.FindExpert.Controllers
                 using (AppDBContext context = new AppDBContext())
                 {
                     var p = new PromotionRepository(context).GetPromotionById(Id);
-                    promotion = new PromotionModel
+
+                    if (p != null)
                     {
-                        Id = p.Id,
-                        Title = p.Title,
-                        Header = p.Header,
-                        Description = p.Description,
-                        CreatedDate = p.CreatedDate?.ToString(Constant.DateFormatType.YYYYMMDD),
-                        Status = p.Status ?? 0,
-                        Type = ((p.Type ?? 0) == Constant.PromotionType.OFFER) ? "Offers" : "Promotions"
-                    };
+                        promotion = new PromotionModel
+                        {
+                            Id = p.Id,
+                            Title = p.Title,
+                            Header = p.Header,
+                            Description = p.Description,
+                            CreatedDate = p.CreatedDate?.ToString(Constant.DateFormatType.YYYYMMDD),
+                            Status = p.Status ?? 0,
+                            Type = ((p.Type ?? 0) == Constant.PromotionType.OFFER) ? "Offers" : "Promotions"
+                        };
+                    }
                 }
             }
             catch (Exception ex)
             {
+                Logger.Log(typeof(PromotionController), ex.Message + ex.StackTrace, LogType.ERROR);
                 return InternalServerError(ex);
             }
 
@@ -74,6 +84,7 @@ namespace IVA.FindExpert.Controllers
 
         [HttpGet]
         [Authorize]
+        [AccessActionFilter]
         public IHttpActionResult GetLatestPromotion(int Type)
         {
             PromotionModel promotion = null;
@@ -82,20 +93,24 @@ namespace IVA.FindExpert.Controllers
                 using (AppDBContext context = new AppDBContext())
                 {
                     var p = new PromotionRepository(context).GetLatestPromotion(Type);
-                    promotion = new PromotionModel
+                    if (p != null)
                     {
-                        Id = p.Id,
-                        Title = p.Title,
-                        Header = p.Header,
-                        Description = p.Description,
-                        CreatedDate = p.CreatedDate?.ToString(Constant.DateFormatType.YYYYMMDD),
-                        Status = p.Status ?? 0,
-                        Type = ((p.Type ?? 0) == Constant.PromotionType.OFFER) ? "Offers" : "Promotions"
-                    };
+                        promotion = new PromotionModel
+                        {
+                            Id = p.Id,
+                            Title = p.Title,
+                            Header = p.Header,
+                            Description = p.Description,
+                            CreatedDate = p.CreatedDate?.ToString(Constant.DateFormatType.YYYYMMDD),
+                            Status = p.Status ?? 0,
+                            Type = ((p.Type ?? 0) == Constant.PromotionType.OFFER) ? "Offers" : "Promotions"
+                        };
+                    }
                 }
             }
             catch (Exception ex)
             {
+                Logger.Log(typeof(PromotionController), ex.Message + ex.StackTrace, LogType.ERROR);
                 return InternalServerError(ex);
             }
 
